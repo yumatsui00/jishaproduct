e767f8fa525c6be36c85cbc13a6f0cb617ea4c46
ゆま <yumasan.122700@gmail.com>
2026-03-17 17:20:01 +0900

    feat: create LP inital

---
+++ assets/translations/jp.ts
@@ -0,0 +1,20 @@
+interface HomePageTranslations {
+  home: {
+    hero: {
+      title: string;
+      description: string;
+    };
+  };
+}
+
+const translations: HomePageTranslations = {
+  home: {
+    hero: {
+      title: "Jisha project has started.",
+      description:
+        "This is the initial top page for the Next.js application.",
+    },
+  },
+};
+
+export default translations;
